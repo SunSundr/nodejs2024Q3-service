27cd7f7d98a053180d566ab95e190b8e2b48838e
SunSundr <art.dizigner@gmail.com>
2024-11-12 03:46:17 +0400

    refactor: change runSwagger

--- src/common/swagger/runSwagger.ts
+++ src/common/swagger/runSwagger.ts
@@ -2,13 +2,15 @@ import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
 import { APP_NAME, SWAGGER_PATH, SWAGGER_USE_DEFAULT_SCHEMA } from 'src/app.config';
 import { readFileSync } from 'fs';
-import * as yaml from 'js-yaml';
+import { resolve } from 'path';
+import { cwd } from 'process';
+import { parse } from 'yaml';
 
 export function runSwagger(app: INestApplication): void {
   let document: OpenAPIObject;
   if (SWAGGER_USE_DEFAULT_SCHEMA) {
-    const swaggerYaml = readFileSync('./doc/api.yaml', 'utf8');
-    document = yaml.load(swaggerYaml) as OpenAPIObject;
+    const swaggerYaml = readFileSync(resolve(cwd(), 'doc', 'api.yaml'), 'utf8');
+    document = parse(swaggerYaml);
   } else {
     const config = new DocumentBuilder()
       .setTitle(APP_NAME)
